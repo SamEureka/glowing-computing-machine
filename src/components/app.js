54374cb429258ed6791da481678ade427109f4b4
SamEureka <sameureka@me.com>
2016-03-29 15:51:53 -0400

    going home

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+        {this.props.children}
+      </div>
     );
   }
 }
---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Secret Boom Time!
+      <ul>
+        <li>Do something cool!</li>
+        <li>Be radical!</li>
+        <li>Yeah!</li>
+      </ul>
+    </div>
+  );
+};
